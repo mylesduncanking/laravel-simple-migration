07c94d1f1b3e6f22b1072e60e4e6e55273da0c0c
Myles Duncan-King <mylesduncanking@hotmail.com>
2022-07-13 23:02:17 +0100

    Adjustments to work with new layout

--- src/SimpleMigration.php
+++ src/SimpleMigration.php
@@ -11,34 +11,37 @@ class SimpleMigration extends Migration
     public function up()
     {
         foreach ($this->migration as $tableName => $columns) {
-            $schemaMethod = SchemaMethod::get($tableName, $columns);
-        }
-        $schemaMethod = property_exists($this, 'table') ? 'table' : 'create';
-        $tableName    = $this->$schemaMethod;
+            list($schemaMethod, $tableName) = SchemaMethod::get($tableName, $columns);
 
-        Schema::$schemaMethod($tableName, function (Blueprint $table) {
-            foreach ($this->migration as $typeName => $options) {
-                list($type, $args) = MethodArgs::get($typeName, 'string');
-                $column = call_user_func_array([$table, $type], $args);
+            Schema::$schemaMethod($tableName, function (Blueprint $table) use ($columns) {
+                foreach ($columns as $typeName => $options) {
+                    list($type, $args) = MethodArgs::get($typeName, 'string');
+                    $column = call_user_func_array([$table, $type], $args);
 
-                foreach ($options as $option) {
-                    list($method, $args) = MethodArgs::get($option);
-                    call_user_func_array([$column, $method], $args);
+                    foreach ($options as $option) {
+                        list($method, $args) = MethodArgs::get($option);
+                        call_user_func_array([$column, $method], $args);
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 
     public function down()
     {
-        if (property_exists($this, 'create')) {
-            Schema::dropIfExists($this->create);
-        } else {
-            Schema::table($this->table, function (Blueprint $table) {
-                foreach (array_reverse(array_keys($this->migration)) as $typeName) {
+        foreach ($this->migration as $tableName => $columns) {
+            list($schemaMethod, $tableName) = SchemaMethod::get($tableName, $columns);
+
+            if ($schemaMethod == 'create') {
+                Schema::dropIfExists($tableName);
+                continue;
+            }
+
+            Schema::table($tableName, function (Blueprint $table) use ($tableName, $columns) {
+                foreach (array_reverse(array_keys($columns)) as $typeName) {
                     $column = MethodArgs::get($typeName, 'string')[1][0];
 
-                    if (Schema::hasColumn($this->table, $column)) {
+                    if (Schema::hasColumn($tableName, $column)) {
                         $table->dropColumn($column);
                     }
                 }
